a7c50deba229d13755ed68f142b23be52e807da1
Magikmagnum <ericgansa01@gmail.com>
2022-06-20 12:46:24 +0200

    Ecoute et publication sur le topic du SSI

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { mqttPublisher } = require('./services/mqttService/publisher');
 const { mqttSubscriber } = require('./services/mqttService/subscriber');
 
-const topic = '/nodejs/mqtt';
+const topic = 'HUB/TAIIB1030432';
 
-mqttPublisher(topic, 'nodejs mqtt coucou');
+//mqttPublisher(topic, 'nodejs mqtt coucou');
 mqttSubscriber(topic);
