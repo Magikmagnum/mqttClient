82ee8515a877c3ea530b9c2c46764beeb2bac4f0
Magikmagnum <ericgansa01@gmail.com>
2022-06-21 08:20:03 +0200

    Implementation du serveur Web node js

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+// On instancie express
+const app = require('express')();
+// On cree le serveur HTTP; 
+const http = require('http').createServer(app);
+// On instancie le websoket http
+const io = require('socket.io')(http);
+
+
+
+
+
+// On ecoute l'evenement "connection" de socket.io
+io.on('connection', (socket) => {
+    console.log(socket)
+})
+
+// On cree une route index
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+// On precise le port de reponse http sur le 3000
+http.listen(3000, () => {
+    console.log("j'ecoute le port 3000");
+})
+
+
+/*
+
 const { mqttPublisher } = require('./services/mqttService/publisher');
 const { mqttSubscriber } = require('./services/mqttService/subscriber');
 
-const topic = 'HUB/TAIIB1030432';
+const topic = '#';
 
-//mqttPublisher(topic, 'nodejs mqtt coucou');
+mqttPublisher(topic, 'nodejs mqtt coucou');
 mqttSubscriber(topic);
+
+*/
